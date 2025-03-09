db2859b156de034d8c3bb1caba8f1d05355c30d2
JasonGrace2282 <aarush.deshpande@gmail.com>
2025-03-08 22:11:23 -0500

    so I tried locking in

--- backend/credibly/apps/api/tasks.py
+++ backend/credibly/apps/api/tasks.py
@@ -1,19 +1,48 @@
-import os
+from typing import Annotated
 
 from celery import shared_task
-from newsapi import NewsApiClient
+from pydantic import BaseModel, Field
+from google import genai
+from google.genai import types
 
 from .models import BiasedContent
-
 
 
 @shared_task
 def check_validity_of_info(info: str, content_id: int):
     """Sets the accuracy of the content based on the validity of the info."""
-    # TODO: figure out if the info is trying to state a fact
-
     content = BiasedContent.objects.get(id=content_id)
-    api_key = os.environ.get('NEWS_API_KEY')
-    if not api_key:
-        raise ValueError('NEWS_API_KEY environment variable not set')
-    client = NewsApiClient(api_key=api_key)
+
+    gemini = genai.Client(api_key="AIzaSyCoFeqRzGX9VUr6OJHWZJxbkpV54oGt0-g")
+    response = gemini.models.generate_content(
+        model="gemini-2.0-flash",
+        contents=(
+            "What is the amount of misinformation in this statement, on a scale of 0 to 1?"
+            f"How certain are you in your decision on a scale of 0 to 1?\n{info!r}"
+        ),
+        config=types.GenerateContentConfig(
+            response_mime_type="application/json",
+            response_schema=CheckBSResponse,
+            tools=[types.Tool(google_search=types.GoogleSearch())],
+        ),
+    )
+
+    if isinstance(response.parsed, dict):
+        parsed = CheckBSResponse.model_validate_json(response.parsed)
+    else:
+        parsed = response.parsed
+    assert isinstance(parsed, CheckBSResponse)
+
+    print("Gemini info: ", parsed)
+    if parsed.certainity < 0.5:
+        return
+
+    content.accuracy = 1 - parsed.misinformation_amount
+    content.save(update_fields=["accuracy"])
+
+
+type zero_to_one = Annotated[float, Field(ge=0, le=1)]
+
+class CheckBSResponse(BaseModel):
+    misinformation_amount: zero_to_one
+    certainity: zero_to_one
