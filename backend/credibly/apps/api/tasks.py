f025055ab53658fbf2e778ad431f622e492b9ff6
Aarush Chaurasia <thisisaarush@gmail.com>
2025-03-09 05:57:10 -0400

    task to view

--- backend/credibly/apps/api/tasks.py
+++ backend/credibly/apps/api/tasks.py
@@ -9,56 +9,6 @@ from pydantic_core import from_json
 from .models import BiasedContent
 
 
-@shared_task
-def check_validity_of_info(content_id: int):
-    """Sets the accuracy of the content based on the validity of the info."""
-    content = BiasedContent.objects.get(id=content_id)
-    info = content.content
-
-    prompt = (
-        "{"
-        '\t"misinformation_score": "What is the amount of misinformation in this statement, on a scale of 0 to 1?",'
-        '\t"certainty_score": "How certain are you in your decision on a scale of 0 to 1?"'
-        "}\n"
-        f"The output should strictly follow the JSON schema: {CheckBSResponse.model_json_schema()}\n"
-        f"The input statement is: {info!r}\n"
-        "Please provide the output strictly in JSON format, and do not include any additional information or analysis."
-    )
-
-    gemini = genai.Client(api_key="AIzaSyCoFeqRzGX9VUr6OJHWZJxbkpV54oGt0-g")
-    response = gemini.models.generate_content(
-        model="gemini-2.0-flash",
-        contents=prompt,
-        config=types.GenerateContentConfig(
-            tools=[types.Tool(google_search=types.GoogleSearch())],
-        ),
-    )
-    if response.candidates is None:
-        print("GEMINI GOT COOKED GODDAMN IT")
-        return
-
-    gemini_content = response.candidates[0].content
-    if gemini_content is None or gemini_content.parts is None:
-        print("I'm gonna jump off a cliff FUK\n", response)
-        return
-
-    text = gemini_content.parts[0].text
-    if text is None:
-        print("I give up ", response)
-        return
-
-    text = text.strip("`").removeprefix("json")
-    parsed = CheckBSResponse.model_validate(from_json(text, allow_partial=True))
-
-    print("Gemini info: ", parsed)
-    if parsed.certainity < 0.5:
-        print("Gemini not sure")
-        return
-
-    content.accuracy = 1 - parsed.misinformation_amount
-    content.accuracy_certainity = parsed.certainity
-    content.save(update_fields=["accuracy"])
-
 
 type zero_to_one = Annotated[float, Field(ge=0, le=1)]
 
--- backend/credibly/apps/api/views.py
+++ backend/credibly/apps/api/views.py
@@ -10,6 +10,7 @@ import easyocr
 import cv2
 
 from django.http import JsonResponse
+from pydantic_core._pydantic_core import from_json
 from rest_framework.decorators import api_view
 from rest_framework.renderers import JSONRenderer
 from django.db import models
@@ -24,7 +25,8 @@ from .serializers import (
     ContentCreatorSerializer,
 )
 from .models import ContentCreator, BiasedMedia
-from .tasks import check_validity_of_info
+from google.genai import types
+import google.genai as genai
 
 
 reader = easyocr.Reader(["en"])
@@ -70,9 +72,64 @@ def good_content_creators(request):
 def start_analysis_of_statements(request):
     m = BiasedMedia.objects.get(url=request.POST["video_url"])
     for content in m.biased_content.all():
-        check_validity_of_info.delay(content.id)
+        check_validity_of_info(content_id)
     return JsonResponse({"status": "Analysis started"})
 
+def check_validity_of_info(id):
+    """Sets the accuracy of the content based on the validity of the info."""
+    content = BiasedContent.objects.get(id=id)
+    info = content.content
+
+    prompt = (
+        "{"
+        '\t"misinformation_score": "What is the amount of misinformation in this statement, on a scale of 0 to 1?",'
+        '\t"certainty_score": "How certain are you in your decision on a scale of 0 to 1?"'
+        "}\n"
+        f"The output should strictly follow the JSON schema: {CheckBSResponse.model_json_schema()}\n"
+        f"The input statement is: {info!r}\n"
+        "Please provide the output strictly in JSON format, and do not include any additional information or analysis."
+    )
+
+    gemini = genai.Client(api_key="AIzaSyCoFeqRzGX9VUr6OJHWZJxbkpV54oGt0-g")
+    response = gemini.models.generate_content(
+        model="gemini-2.0-flash",
+        contents=prompt,
+        config=types.GenerateContentConfig(
+            tools=[types.Tool(google_search=types.GoogleSearch())],
+        ),
+    )
+    if response.candidates is None:
+        print("GEMINI GOT COOKED GODDAMN IT")
+        return
+
+    gemini_content = response.candidates[0].content
+    if gemini_content is None or gemini_content.parts is None:
+        print("I'm gonna jump off a cliff FUK\n", response)
+        return
+
+    text = gemini_content.parts[0].text
+    if text is None:
+        print("I give up ", response)
+        return
+
+    text = text.strip("`").removeprefix("json")
+    parsed = CheckBSResponse.model_validate(from_json(text, allow_partial=True))
+
+    print("Gemini info: ", parsed)
+    if parsed.certainity < 0.5:
+        print("Gemini not sure")
+        return
+
+    content.accuracy = 1 - parsed.misinformation_amount
+    content.accuracy_certainity = parsed.certainity
+    content.save(update_fields=["accuracy"])
+
+
+type zero_to_one = Annotated[float, Field(ge=0, le=1)]
+
+class CheckBSResponse(BaseModel):
+    misinformation_amount: zero_to_one
+    certainity: zero_to_one
 
 @api_view(["POST"])
 def credibility_view(request, url):
